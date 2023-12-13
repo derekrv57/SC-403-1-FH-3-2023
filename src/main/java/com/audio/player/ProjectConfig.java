b484ef6b9b646f6cc3593303b46e05f3ee45f072
Jorge Sayegh <jorgelias995@gmail.com>
2023-12-13 13:18:21 -0600

    Login

--- src/main/java/com/audio/player/ProjectConfig.java
+++ src/main/java/com/audio/player/ProjectConfig.java
@@ -4,58 +4,58 @@
  */
 package com.audio.player;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- *
- * @author jorge
- */
-public class ProjectConfig implements WebMvcConfigurer {
-        @Bean
+
+@Configuration
+@EnableWebSecurity
+public class ProjectConfig {
+
+    @Bean
     public UserDetailsService users() {
-        
         UserDetails admin = User.builder()
                 .username("ADMIN")
-                .password("{noop}ACCESS")
+                .password("$2a$10$S2m5ZHyI68/Z3JGF6B2Jt.0eDk/mr8AfP2kD9YUG0tEl8qFdmuOyC") // BCrypt encoded password
                 .roles("USUARIO", "ADMIN")
                 .build();
         UserDetails artist = User.builder()
                 .username("ARTIST")
-                .password("{noop}ACCESS")
+                .password("$2a$10$S2m5ZHyI68/Z3JGF6B2Jt.0eDk/mr8AfP2kD9YUG0tEl8qFdmuOyC") // BCrypt encoded password
                 .roles("USUARIO")
                 .build();
-        return new InMemoryUserDetailsManager( artist, admin);
+        return new InMemoryUserDetailsManager(artist, admin);
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http        
-                .authorizeHttpRequests((request) -> request
-                .requestMatchers("/","/account/**","/album/**",
-                        "/artist/**","/index/**","/join/**",
-                        "/login/**","/playlist/**","/song/**","/logout/**")
+        http
+                .authorizeRequests((requests) -> requests
+                        .antMatchers("/", "/account/**", "/album/**",
+                                "/artist/**", "/index/**", "/join/**",
+                                "/login/**", "/playlist/**", "/song/**", "/logout/**")
                         .hasAnyRole("USUARIO", "ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                .loginPage("/login").permitAll())
+                        .loginPage("/login").permitAll())
                 .logout((logout) -> logout.permitAll());
         return http.build();
     }
-     @Autowired
-    private UserDetailsService userDetailsService;
 
-    @Autowired
-    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
-        build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-    
 }
+
+
